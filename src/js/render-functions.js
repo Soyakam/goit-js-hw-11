017b324eeafe63da8f6933b0d67afa1c3bd60b1d
Soyakam <108721019+Soyakam@users.noreply.github.com>
2024-04-06 08:58:42 +0200

    xxx

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,49 +1,43 @@
-import iziToast from 'izitoast';
-import 'izitoast/dist/css/iziToast.min.css';
-import SimpleLightbox from 'simplelightbox';
-import 'simplelightbox/dist/simple-lightbox.min.css';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
-const galleryList = document.querySelector('ul.gallery');
+export const galleryEl = document.querySelector(".gallery");
+export function createMarkUp(images) {
+  const markUp = images
+    .map(
+      image => `<li class="gallery-item">
+  <a class="gallery-link"
+    href="${image.largeImageURL}"
+    ><img
+      src="${image.webformatURL}"
+      alt="${image.tags}"
+  />
+  <ul class="img-dscr">
+    <li>
+      <p><b>Likes</b> ${image.likes}</p>
+    </li>
+    <li>
+      <p><b>Views</b> ${image.views}</p>
+    </li>
+    <li>
+      <p><b>Comments</b> ${image.comments}</p>
+    </li>
+    <li>
+      <p><b>Downloads</b> ${image.downloads}</p>
+    </li>
+  </ul>
+  </a>
+</li>
 
-export function renderImages(data) {
-  galleryList.innerHTML = '';
-  const images = data.hits;
-  if (images.length === 0) {
-    iziToast.error({
-      title: 'Error',
-      message: `Error: No such pictures!`,
-      position: 'topRight',
-    });
-  } else {
-    const galleryMarkup = images
-      .map(image => {
-        return `<li class="gallery-item">
-          <a class="gallery-link" href="${image.largeImageURL}">
-            <img
-              class="gallery-image"
-              width="1280"
-              height="152"
-              src="${image.webformatURL}"
-              data-source="${image.largeImageURL}"
-              alt="${image.tags}"
-            />
-            <ul class="gallery-description">
-              <li><h3>Likes</h3><p>${image.likes}</p></li>
-              <li><h3>Views</h3><p>${image.views}</p></li>
-              <li><h3>Comments</h3><p>${image.comments}</p></li>
-              <li><h3>Downloads</h3><p>${image.downloads}</p></li>
-            </ul>
-          </a>
-        </li>`;
-      })
-      .join('');
-    galleryList.insertAdjacentHTML('beforeend', galleryMarkup);
-    // Initialize SimpleLightbox
-    const galleryCfg = {
-      captionsData: 'alt',
-    };
-    let lightbox = new SimpleLightbox('.gallery a', galleryCfg);
-    lightbox.on('show.simplelightbox', function () {});
-    lightbox.refresh();
-  }
+`
+    )
+    .join("");
+  galleryEl.insertAdjacentHTML("beforeend", markUp);
+  const lightbox = new SimpleLightbox(".gallery-link", {
+    captionsData: "alt",
+    captionDelay: 250,
+  });
 }
+
+
+
--- src/main.js
+++ src/main.js
@@ -1,34 +1,55 @@
-import iziToast from 'izitoast';
-import 'izitoast/dist/css/iziToast.min.css';
-import { fetchImages } from './js/pixabay-api';
-import { renderImages } from './js/render-functions';
+import "./js/pixabay-api";
+import { getImages } from "./js/pixabay-api";
+import "./js/render-function";
+import { createMarkUp } from "./js/render-function";
+import { galleryEl } from "./js/render-function";
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
-const searchForm = document.getElementById('search-form');
-const inputQuery = document.getElementById('search-input');
+const formEl = document.querySelector(".form");
+const inputEl = document.querySelector("input");
+const loaderEl = document.querySelector(".loader");
 
-searchForm.addEventListener('submit', e => {
-  e.preventDefault();
-  const query = inputQuery.value.trim();
-  if (query) {
-    fetchImages(query)
-      .then(data => {
-        if (data.hits.length === 0) {
-          iziToast.error({
-            title: 'Error',
-            message: `Error: No such pictures!`,
-            position: 'topRight',
-          });
-        } else {
-          renderImages(data);
-        }
-      })
-      .catch(error => {
-        console.error(error);
-        iziToast.error({
-          title: 'Error',
-          message: `An error occurred while loading images. Please try again later.`,
-          position: 'topRight',
-        });
-      });
+formEl.addEventListener("submit", event => {
+  event.preventDefault();
+  loaderEl.classList.add("is-open");
+  galleryEl.innerHTML = "";
+  lightbox.refresh();
+  const queryTrimed = inputEl.value.trim();
+  if (queryTrimed === "") {
+    loaderEl.classList.remove("is-open");
+    return iziToast.warning({
+      title: "Caution",
+      message: "Please complete the field!",
+      position: "topRight",
+    });
   }
+  getImages(queryTrimed)
+    .then(response => {
+      if (response.hits.length === 0) {
+        return iziToast.info({
+          title: "Sorry",
+          message:
+            "There are no images matching your search query. Please try again!",
+          position: "topRight",
+        });
+      }
+      createMarkUp(response.hits);
+    })
+    .catch(error => {
+      console.error("Error fetching images:", error);
+    })
+    .finally(() => loaderEl.classList.remove("is-open"));
+  inputEl.value = "";
 });
+
+const lightbox = new SimpleLightbox(".gallery-link", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
+
+
+
+
